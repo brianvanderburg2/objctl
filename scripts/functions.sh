363a098c80eff6caa2bdb2a60764c8cdf6fa80a4
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2015-04-08 00:20:53 -0400

    Minor change to functions.sh

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -4,8 +4,10 @@
 # Run a command, display if verbose
 run()
 {
-    [ "$VERBOSE" = "0" ] || echo $@
-    $@
+    # Change spaces to be escaped in the display so the user can
+    # determine that multiple words may be a single argument.
+    [ "$VERBOSE" = "0" ] || echo ${@//\ /\\\ }
+    "$@"
 }
 
 
